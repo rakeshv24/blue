4bfa5fa5b8a3f02820dbe565426da46c9b3c8132
Rakesh Vivekanandan <rakeshvivek97@gmail.com>
2026-06-30 18:18:51 +0000

    minor edits

--- blue_demos/control_integration/launch/bluerov2_heavy_controllers.launch.py
+++ blue_demos/control_integration/launch/bluerov2_heavy_controllers.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2024, Evan Palmer
+# Copyright 2026, Rakesh Vivekanandan
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -92,6 +92,7 @@ def generate_launch_description() -> LaunchDescription:
                 ]
             ),
         ],
+        # arguments=["--ros-args", "--log-level", "robust_mpc:=debug"],
     )
 
     def make_controller_args(
